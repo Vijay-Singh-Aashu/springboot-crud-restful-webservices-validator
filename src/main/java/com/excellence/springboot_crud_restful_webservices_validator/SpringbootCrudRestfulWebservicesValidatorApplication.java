6b9b7f8f8709cdc5f527716c943afb0405d06062
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-02 10:47:43 +0530

    base-code-created

---
+++ src/main/java/com/excellence/springboot_crud_restful_webservices_validator/SpringbootCrudRestfulWebservicesValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.springboot_crud_restful_webservices_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestfulWebservicesValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulWebservicesValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence/springboot_crud_restful_webservices_validator/SpringbootCrudRestfulWebservicesValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.springboot_crud_restful_webservices_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudRestfulWebservicesValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
